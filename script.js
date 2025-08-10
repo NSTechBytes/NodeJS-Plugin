500c23ef0edbd8888cbdaab54210dac125b50482
NSTechBytes <nstechbytes@gmail.com>
2025-08-10 16:25:33 +0500

    improved cleanup

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       });
     },
-    { rootMargin: "-40% 0px -60% 0px" }
+    { rootMargin: "-40% 0px 0px 0px" } // Adjusted rootMargin here
   );
 
   sections.forEach((section) => {
@@ -106,7 +106,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const codeBlock = container.querySelector("pre code");
     const copyButton = document.createElement("button");
     copyButton.className = "copy-btn";
-    copyButton.innerHTML = `<svg class="w-4 h-4 mr-1 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path></svg> Copy`;
+    copyButton.innerHTML = `<svg class="w-4 h-4 mr-1 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path></svg> Copy`;
     container.appendChild(copyButton);
 
     copyButton.addEventListener("click", () => {
